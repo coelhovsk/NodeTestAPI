b68ff2dce24eb67e69c9dffdec12556f1412b197
Henrique <henriquecoelhodossantos2007@gmail.com>
2025-08-11 11:14:27 -0300

    router

--- routes/router.js
+++ routes/router.js
@@ -4,5 +4,21 @@ import { getAllItems } from "../controllers/controller.js"
 
 export const router = Router();
 
-router.get('/', getAllItems)
+router.get('/items', (req, res) => {
+    const items = getAllItems
+    res.json(items)
+})
 
+router.get('/items/:id', (req, res) => {
+  const item = getItemById(req.params.id);
+  if (item) {
+    res.json(item);
+  } else {
+    res.status(404).json({ message: 'Item não encontrado' });
+  }
+});
+
+router.post('/items', (req, res) => {
+  const newItem = addItem(req.body);
+  res.status(201).json(newItem);
+});
