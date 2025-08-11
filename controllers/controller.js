2fc10a8b62f1b08130b19de0ca11cfba758967aa
Henrique <henriquecoelhodossantos2007@gmail.com>
2025-08-11 11:12:00 -0300

    a

--- controllers/controller.js
+++ controllers/controller.js
@@ -4,3 +4,11 @@ import { sampleData } from "../data/sampledata.js";
 export function getAllItems() {
   return sampleData;
 }
+
+export function getItemById (id) { return sampleData.find(item => item.id === parseInt(id))}
+
+export function addItem (newItem) { 
+    const item = { id: sampleData.length + 1, ...newItem }
+    sampleData.push(item)
+    return item
+}
