64c8db059f4e4e573d28c6a3b61fdf80751aa39c
Henrique <henriquecoelhodossantos2007@gmail.com>
2025-08-11 10:41:01 -0300

    Sample

---
+++ data/sampledata.js
@@ -0,0 +1,6 @@
+const sampleData = [
+  { id: 1, name: 'Item 1', description: 'Item do joão' },
+  { id: 2, name: 'Item 2', description: 'Item do jogao do item 2' },
+  { id: 3, name: 'Item 3', description: '3 Joao do do item 3' },
+  { id: 4, name: 'Item 4', description: '4 Item do od joao 4' },
+];
