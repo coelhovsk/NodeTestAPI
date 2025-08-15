be14804bc0fd3121214ed4e76556209bf0d2bd28
Henrique <henriquecoelhodossantos2007@gmail.com>
2025-08-15 09:56:42 -0300

    error

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use('/', dataRoutes);
 
 // Rota para servir o index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.listen(3000, () => {
